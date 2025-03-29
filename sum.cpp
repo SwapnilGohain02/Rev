1e3a19d221cc3e59db2fa8878f234ddf85efd38d
Swapnil Burgaohain <swapnilgohan07@gmail.com>
2025-03-29 11:06:26 +0530

    changes

--- sum.cpp
+++ sum.cpp
@@ -5,6 +5,7 @@ int main(){
     int a,b;
     cout<<"Enter the values of a and b respectively:";
     cin>>a>>b;
-    cout<<"The required sum is: "<<a+b;
+    cout<<"The required sum is: "<<a+b<<endl;
+    cout<<"Hope you find it helpful";
     return 0;
 }
