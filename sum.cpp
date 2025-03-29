257c2173f6a27853e7c370466a935c3732563e1b
Swapnil Burgaohain <swapnilgohan07@gmail.com>
2025-03-29 11:01:06 +0530

    Add the file

---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the values of a and b respectively:";
+    cin>>a>>b;
+    cout<<"The required sum is: "<<a+b;
+    return 0;
+}
